e22c7c66493d3d263803891868353f7a5a8abef6
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-10-20 10:14:06 -0700

    fixed setOptions render warning

--- components/Chat.js
+++ components/Chat.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default class Chat extends React.Component {
-  render() {
+  compnentDidMount() {
     let name = this.props.route.params.name;
-    // let bgColor
     this.props.navigation.setOptions({ title: name });
+  }
+  render() {
     return (
       <View style={{ flex: 1, backgroundColor: this.props.route.params.bgColor }}>
       </View >
