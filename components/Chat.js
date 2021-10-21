34319e052d0094fdb517bfcb128be650e9736b0d
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-10-20 18:48:43 -0700

    added chat interaction and cloud firebase storage

--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,134 @@
 import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat'
+
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      uid: 0,
+      user: {
+        _id: '',
+        name: ''
+      }
+    }
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyArhsk699cfu-tiQd8hTYr7Fpu_yi3RHtE",
+      authDomain: "chat-app-2748b.firebaseapp.com",
+      projectId: "chat-app-2748b",
+      storageBucket: "chat-app-2748b.appspot.com",
+      messagingSenderId: "688325133871",
+      appId: "1:688325133871:web:e8979f12c173b2e4b0a8da",
+      measurementId: "G-GVRQ1TP9Q9"
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+  }
+
   componentDidMount() {
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+      });
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
   }
+
+  componentWillUnmount() {
+    // stop listening to authentication
+    this.authUnsubscribe();
+    // stop listening for changes
+    this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        createdAt: data.createdAt.toDate(),
+        text: data.text || '',
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        }
+      });
+    });
+
+    this.setState({
+      messages
+    });
+  }
+
+  addMessages() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      uid: this.state.uid,
+      text: message.text,
+      createdAt: message.createdAt,
+      user: message.user,
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }),
+      () => {
+        this.addMessages();
+      }
+    )
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: this.props.route.params.bgColor }}>
+      <View style={{ flex: 1 }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: this.state.uid,
+            name: this.props.route.params.name
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View >
     )
   }
